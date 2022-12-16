c36f9f6f4a9db05641f4c0dbb9f718831c41c12e
ducle.canh <ducle.canh@bytedance.com>
2022-12-16 13:45:34 +0800

    update dockerfile

--- build/build.sh
+++ build/build.sh
@@ -9,8 +9,13 @@ apt install -y strip
 mkdir -p "${APP_ROOT}"
 curl --retry 3 "http://d.scm.byted.org/api/v2/download/ceph:$(echo "${SCM_REPO}" | sed "s/\//./g")_${SCM_VERSION}.tar.gz" | tar -xz -C "${APP_ROOT}"
 
-# strip the binary
-strip ${APP_ROOT}/bin/clickhouse*
+# remove unnecessary files
+rm -rf ${APP_ROOT}/clickhouse-library-bridge
+rm -rf ${APP_ROOT}/clickhouse-odbc-bridge
+
+# strip the binary - skip for debug symbols
+strip ${APP_ROOT}/bin/clickhouse
+strip ${APP_ROOT}/lib/libfdb_c.so
 
 # clean
 rm -rf /build
